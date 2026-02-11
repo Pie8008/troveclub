649271ab7f8523197981d91ee3cdc553faf414e3
Pie8008 <2908077313@qq.com>
2026-02-12 01:18:30 +0800

    大规模翻新

---
+++ json/二级菜单页应用数据.js
@@ -0,0 +1,149 @@
+const appConfigs = {
+    tools: {
+        title: "工具箱",
+        apps: [
+            {
+                name: "园艺计算器",
+                color: "#FF9500",
+                url: "../html/B工具页/计算器.html",
+                openType: "sameTab"
+            },
+            {
+                name: "收获时间表",
+                color: "#FF9500",
+                url: "../html/B工具页/收获时间.html",
+                openType: "sameTab"
+            },
+            {
+                name: "加速时间表",
+                color: "#FF9500",
+                url: "../html/B工具页/加速时间.html",
+                openType: "sameTab"
+            },
+            {
+                name: "金粉计算器",
+                color: "#FF9500",
+                url: "../html/B工具页/金粉计算.html",
+                openType: "sameTab"
+            },
+            {
+                name: "齁哦加密",
+                color: "#5856D6",
+                url: "../html/B工具页/齁哦加密.html",
+                openType: "sameTab"
+            }
+        ]
+    },
+
+    guides: {
+        title: "快捷攻略",
+        apps: [
+            {
+                name: "装备耗材",
+                color: "#FF3B30",
+                url: "../html/C攻略页/装备耗材.html",
+                openType: "sameTab"
+            },
+            {
+                name: "钓鱼地点",
+                color: "#FF3B30",
+                url: "../html/C攻略页/钓鱼地点.html",
+                openType: "sameTab"
+            },
+            {
+                name: "登录错误",
+                color: "#FF3B30",
+                url: "../html/C攻略页/登录错误.html",
+                openType: "sameTab"
+            }
+        ]
+    },
+
+    more: {
+        title: "更多",
+        apps: [{
+            name: "公会攻略",
+            color: "#5856D6",
+            url: "https://promiscuouseden.notion.site/trove-index-aaaaa00000",
+            imageUrl: "../图片/图标/公会图标.png",
+            openType: "newTab"
+        },
+        {
+            name: "TroveSaurus",
+            color: "#5856D6",
+            url: "https://trovesaurus.com/",
+            imageUrl: "../图片/图标/社区图标.png",
+            openType: "newTab"
+        },
+        {
+            name: "TroveWiki",
+            color: "#5856D6",
+            url: "https://trove.fandom.com/wiki/Trove_Wiki",
+            imageUrl: "../图片/图标/宝藏维基图标.webp",
+            openType: "newTab"
+        },
+        {
+            name: "TroveGame",
+            color: "#5856D6",
+            url: "https://trovegame.com/patch-notes/patch-notes-radiant-pathfinder-11-2025/",
+            imageUrl: "../图片/图标/宝藏文字图标.webp",
+            openType: "newTab"
+        },
+        {
+            name: "Glyph官网",
+            color: "#5856D6",
+            url: "https://glyph.trionworlds.com/",
+            imageUrl: "../图片/图标/字形图标.png",
+            openType: "newTab"
+        },
+        {
+            name: "Glyph用户条款",
+            color: "#5856D6",
+            url: "https://www.glyph.net/en/terms-of-use-of-the-glyph-platform/",
+            imageUrl: "../图片/图标/字形图标.png",
+            openType: "newTab"
+        },
+        {
+            name: "神秘小玩意",
+            color: "#5856D6",
+            url: "https://arcxingye.github.io/r18/index.html",
+            imageUrl: "../图片/图标/R18.webp",
+            openType: "newTab"
+        }
+        ]
+    },
+
+    records: {
+        title: "记录",
+        apps: [{
+            name: "画廊",
+            color: "#FF9500",
+            url: "../html/E记录页/画廊.html",
+            openType: "sameTab"
+        },
+        {
+            name: "历史簿",
+            color: "#FF9500",
+            url: "../html/E记录页/记录.html",
+            openType: "sameTab"
+        }
+        ]
+    },
+
+    games: {
+        title: "小游戏",
+        apps: [{
+            name: "贪吃蛇",
+            color: "#FF9500",
+            url: "../html/G游戏页/贪吃蛇.html",
+            openType: "newTab"
+        },
+        {
+            name: "扫雷",
+            color: "#FF9500",
+            url: "../html/G游戏页/扫雷.html",
+            openType: "newTab"
+        }
+        ]
+    }
+};
---
+++ json/导航页面数据.js
@@ -0,0 +1,9 @@
+const PagePaths = {
+    'A': './html/A主页/简介.html',
+    'B': './html/二级菜单页.html?config=tools',
+    'C': './html/二级菜单页.html?config=guides',
+    'D': './html/二级菜单页.html?config=more',
+    'E': './html/二级菜单页.html?config=records',
+    'F': './html/F数据页/物品数据.html',
+    'G': './html/二级菜单页.html?config=games'
+};

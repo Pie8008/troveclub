775611f65d378fd33c832a3ba8b1e4c9e44702fc
PumpkinPie <143673391+Pie8008@users.noreply.github.com>
2026-08-26 21:58:55 +0800

    Delete json directory

--- json/公会历史数据.js
+++
@@ -1,7 +0,0 @@
-const RECORDS = [
-    { id: 1, title: "2024年8月1日", description: "公会由sexeveryday更名为promiscuouseden。" },
-    { id: 2, title: "2024年8月21日", description: "会长Yuqi因在公会重铸金雕被封禁7天。" },
-    { id: 3, title: "2025年3月", description: "Pie8008消失了。" },
-    { id: 4, title: "2025年11月", description: "zLOVEz消失了。" },
-    { id: 5, title: "2025年12月5号", description: "会长Yuqi因被夺舍打广告而封禁7天。" }
-];
